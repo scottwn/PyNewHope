96814e42d6965a9281c9fd6cae51675f9f2f37aa
Scott Wyman Neagle <scott.neagle@cs.nyu.edu>
2017-03-05 21:16:47 -0500

    started writing keygen function

---
+++ newhope.py
@@ -0,0 +1,6 @@
+import poly
+import params
+
+def keygen(send, sk, rand):
+   seed = rand_n(params.NEWHOPE_SEEDBYES) # function returns n random 8-bit unsigned integers
+   a.coeffs = poly.uniform(seed)
--- poly.py
+++ poly.py
@@ -1,7 +1,7 @@
 import params
 import sha3 #need to implement
 
-def poly_uniform(a, seed):
+def uniform(a, seed):
     pos = 0
     ctr = 0
     val = 0
@@ -12,10 +12,12 @@ def poly_uniform(a, seed):
     while ctr < params.N:
         val = (buf[pos] | buf[pos + 1] << 8) & 0x3fff
         if val < params.Q:
-            a.coeffs[ctr++] = val
-            #a is a polynomial, need to write this class
+            coeffs[ctr++] = val
+            # polynomial coefficients for passing to polynomial object in
+            # keygen function
         pos += 2
         if pos > sha3.RATE * nblocks - 2:
             nblocks = 1
             buf = sha3.squeezeblocks(nblocks, state)
             pos = 0
+    return coeffs
