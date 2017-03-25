77cd9d5d1721f4d024a9ed1332cf7bfd9929254d
Scott Wyman Neagle <scott.neagle@cs.nyu.edu>
2017-03-25 18:55:52 -0400

    replaced boutique keccak functions with python 3.6 hashlib module

--- newhope.py
+++ newhope.py
@@ -1,6 +1,7 @@
 import poly
 import params
 import os
+import hashlib
 
 def encode_a(coefficients, seed):
     send = poly.to_bytes(coefficients)
@@ -9,14 +10,23 @@ def encode_a(coefficients, seed):
     return send
 
 def keygen(sk):
-    seed = []
-    for i in range(0,params.NEWHOPE_SEEDBYTES):
-        seed.append(os.urandom(1))
-    a.coeffs = poly.uniform(seed)
-    sk.coeffs = poly.get_noise()
-    sk.coeffs = poly.poly_ntt(sk.coeffs)
-    e.coeffs = poly.get_noise()
-    e.coeffs = poly.poly_ntt(e.coeffs)
-    r.coeffs = poly.pointwise(sk.coeffs, a.coeffs)
-    pk.coeffs = poly.add(e.coeffs, r.coeffs)
+    # 2200 bytes from SHAKE-128 function is enough data to get 1024 coefficients
+    # smaller than 5q, from Alkim, Ducas, Pöppelmann, Schwabe section 7:
+    shake_output = hashlib.shake_128().digest(2200)
+    a_coeffs = []
+    for i in range(0,params.N):
+        j = 0
+        coefficient = 5 * params.Q
+        # Reject coefficients that are greater than or equal to 5q:
+        while coefficient >= 5 * params.Q:
+            coefficient = int.from_bytes(
+                shake_output[j * 2:j * 2 + 2], byteorder = 'little')
+            j++
+        a_coeffs.append(coefficient)
+    sk_coeffs = poly.get_noise()
+    sk_coeffs = poly.poly_ntt(sk.coeffs)
+    e_coeffs = poly.get_noise()
+    e_coeffs = poly.poly_ntt(e.coeffs)
+    r_coeffs = poly.pointwise(sk.coeffs, a.coeffs)
+    pk_coeffs = poly.add(e.coeffs, r.coeffs)
     return encode_a(pk.coeffs, seed)
--- poly.py
+++ poly.py
@@ -6,27 +6,6 @@ import os
 QINV = 12287 # -inverse_mod(p,2^18)
 RLOG = 18
 
-def uniform(a, seed):
-    pos = 0
-    ctr = 0
-    val = 0
-    nblocks = 16
-    state = sha3.absorb(seed, params.NEWHOPE_SEEDBYTES)
-    buf = sha3.squeezeblocks(nblocks, state)
-    # TODO: write these keccak functions; they need to return lists
-    while ctr < params.N:
-        val = (buf[pos] | buf[pos + 1] << 8) & 0x3fff
-        if val < params.Q:
-            coeffs[ctr++] = val
-            # polynomial coefficients for passing to polynomial object in
-            # keygen function
-        pos += 2
-        if pos > sha3.RATE * nblocks - 2:
-            nblocks = 1
-            buf = sha3.squeezeblocks(nblocks, state)
-            pos = 0
-    return coeffs
-
 def get_noise():
     buf = []
     for i in range(0,params.N * 4):
